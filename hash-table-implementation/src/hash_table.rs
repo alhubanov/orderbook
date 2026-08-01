e6bda6c75c69ee2c1232bd722fe6118ba078e5be
Aleksandar Hubanov <al.hubanov@gmail.com>
2026-08-01 20:39:19 +0300

    feat: implement first version of a hash table

--- hash-table-implementation/src/hash_table.rs
+++ hash-table-implementation/src/hash_table.rs
@@ -1,47 +1,104 @@
-use std::collections::{VecDeque};
+use std::{collections::VecDeque};
 
-struct HashTable<'a> 
+const ARRAY_SIZE : usize = 130500;
+pub struct HashTable<'a> 
 {
-    keys: [String; 13],
-    values: [i32; 13],
-    history: VecDeque<(&'a String, i32)>
+    keys: [Option<&'a str>; ARRAY_SIZE],
+    values: [Option<isize>; ARRAY_SIZE],
+    update_history: VecDeque<(&'a str, isize)>
 }
 
 impl<'a> HashTable<'a>
 {
     pub fn new() -> Self
     {
-
+        HashTable { keys: [None; ARRAY_SIZE], values: [None; ARRAY_SIZE], update_history: VecDeque::new() }
     }
 
-    pub fn insert(&mut self, key: String, value: i32) -> ()
+    pub fn insert(&mut self, key: &'a str, value: isize) -> ()
     {
+        let pos = self.hash_with_probe(key);
 
+        self.keys[pos] = Some(key);
+        self.values[pos] = Some(value);
+
+        self.update_history.retain(|(present_key, _)| *present_key != key);
+        self.update_history.push_back((key, value));
     }
 
-    pub fn remove(&mut self, key: String) -> ()
+    pub fn remove(&mut self, key: &'a str) -> ()
     {
+        let mut hash_pos : usize = self.hash_with_probe(key);
 
+        if let None = self.keys[hash_pos]
+        {
+            return;
+        }
+        else 
+        {
+            self.keys[hash_pos] = None;
+            self.values[hash_pos] = None;
+        }
+
+        let mut current_pos = hash_pos + 1;
+        current_pos = current_pos % ARRAY_SIZE;
+
+        while let Some(present_key) = self.keys[current_pos]
+        {
+            if Self::hash(present_key) < hash_pos
+            {   
+                self.keys[hash_pos] = Some(present_key);
+                self.values[hash_pos] = self.values[current_pos];
+
+                self.keys[current_pos] = None;
+                self.values[current_pos] = None;
+
+                hash_pos = current_pos;
+            }
+
+            current_pos += 1;
+        }
+
+        self.update_history.retain(|(present_key, _)| *present_key != key);
     }
 
-    pub fn get(&self, key: String) -> i32
+    pub fn get(&self, key: &'a str) -> Option<isize>
     {
-
+        let pos = self.hash_with_probe(key);
+        self.values[pos]
     }
 
-    pub fn get_last(&self) -> (&String, i32) 
+    pub fn get_last(&self) -> Option<&(&'a str, isize)> 
     {
-
+        self.update_history.back()
     }
 
-    pub fn get_first(&self) -> (&String, i32) 
+    pub fn get_first(&self) -> Option<&(&'a str, isize)> 
     {
-
+        self.update_history.front()
     }
 
-    fn hash(key: String) -> u32 
+    fn hash(key: &str) -> usize
     {
+        let mut pos : usize = 0;
+        for character in key.chars()
+        {
+            pos += character as usize;
+            pos = pos % ARRAY_SIZE; 
+        }
 
-    } 
+        pos
+    }
 
+    fn hash_with_probe(&self, key: &str) -> usize
+    {
+        let mut pos : usize = Self::hash(key);
+        while let Some(present_key) = self.keys[pos] && present_key != key
+        {
+            pos = pos + 1;
+            pos = pos % ARRAY_SIZE;
+        }
+
+        pos
+    }
 }
--- hash-table-implementation/src/lib.rs
+++ hash-table-implementation/src/lib.rs
@@ -1,9 +1,31 @@
 use reqwest;
 
-// mod hash_table;
+mod hash_table;
 
 pub fn run() -> () {
-    extract_words_from_txt_file("https://www.gutenberg.org/files/98/98-0.txt");
+    let _words = extract_words_from_txt_file("https://www.gutenberg.org/files/98/98-0.txt");
+
+    let mut hash_table = hash_table::HashTable::new();
+    hash_table.insert("test1", 1);
+    hash_table.insert("test2", 2);
+    hash_table.insert("test3", -3);
+
+    let value_test1 = hash_table.get("test1");
+    println!("{:?}", value_test1);
+
+    let last_value = hash_table.get_last();
+    let first_value = hash_table.get_first();
+
+    println!("{:?}", last_value);
+    println!("{:?}", first_value);
+
+    hash_table.remove("test2");
+    let value_test3 = hash_table.get("test3");
+
+    println!("{:?}", value_test3);
+
+    let value_test2 = hash_table.get("test2");
+    println!("{:?}", value_test2);
 }
 
 // TODO: add unit tests for word extraction
@@ -28,7 +50,7 @@ fn extract_words_from_txt_file(url: &str) -> Vec<String> // I am assuming I am n
                                 .filter(|word| !word.is_empty())
                                 .collect();
 
-    println!("{:?}", words);
+    // println!("{:?}", words);
 
     words
 }
