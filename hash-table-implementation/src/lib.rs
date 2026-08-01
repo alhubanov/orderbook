4b0ad288f87c0d58f84093d906cd2b1dcad69712
Aleksandar Hubanov <al.hubanov@gmail.com>
2026-08-01 15:43:46 +0300

    feat: improve text parsing function

--- hash-table-implementation/src/lib.rs
+++ hash-table-implementation/src/lib.rs
@@ -6,23 +6,29 @@ pub fn run() -> () {
     extract_words_from_txt_file("https://www.gutenberg.org/files/98/98-0.txt");
 }
 
-fn extract_words_from_txt_file(url: &str) -> ()
+// TODO: add unit tests for word extraction
+// TODO: add error handling
+// TODO: reduce the amount of allocations
+// TODO: determine how to remove the apostrophe character (') correctly.
+
+fn extract_words_from_txt_file(url: &str) -> Vec<String> // I am assuming I am not allowed to use a hash set from the standard library.
 {
     let response_body = reqwest::blocking::get(url).expect("Get request failed.");
-    let text_content = response_body.text().expect("Coundn't extract words from response body.");
+    let text_content = response_body.text().expect("Couldn't extract words from response body.");
     let words : Vec<String> = text_content
+                                .replace(&['(', ')', ',', '.', ';', ':', '\"', '“', '”', '’', '‘', '*', '!', '?'], "")
                                 .lines()
-                                .map(|line| line.replace(&['(', ')', ',', '.', ';', ':', '\"', '“', '”', '’', '‘', '*', '!', '?'], ""))
-                                .filter(|line_string| !line_string.is_empty())
                                 .map(|line| line
-                                        .trim()
-                                        .to_string())
-                                .map(|line| line
+                                        .replace("--", " --")
+                                        .replace("--", "")
                                         .split(" ")
                                         .map(|string| string.to_owned())
                                         .collect::<Vec<String>>())
                                 .flatten()
+                                .filter(|word| !word.is_empty())
                                 .collect();
 
     println!("{:?}", words);
+
+    words
 }
